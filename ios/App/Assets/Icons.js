478423eac5a8fbfebb653e0b9053e4573c990a37
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 06:40:11 -0400

    fix: apply solution for issue #85613

---
+++ ios/App/Assets/Icons.js
@@ -0,0 +1,7 @@
+import { requireNativeComponent } from 'react-native';
+
+export default {
+  Home: require('./home-outlined.png'),
+  Profile: require('./profile-outlined.png'),
+  Settings: require('./settings-outlined.png'),
+};
