111027005779a518b8e801729b12350f1d3dcca8
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-19 19:19:18 -0400

    fix: apply solution for issue #84251

---
+++ src/styles/styles.js
@@ -0,0 +1,12 @@
+    },
+
+    archivedRoomBanner: {
+        backgroundColor: '#f8d7da',
+        padding: 10,
+        alignItems: 'center',
+    },
+
+    archivedRoomBannerText: {
+        color: '#721c24',
+        fontSize: 14,
+    },
