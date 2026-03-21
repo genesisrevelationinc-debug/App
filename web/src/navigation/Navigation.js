c252772e955442c78756da6075577348bd393eb5
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 20:24:32 -0400

    fix: apply solution for issue #75905

--- web/src/navigation/Navigation.js
+++ web/src/navigation/Navigation.js
@@ -3,9 +3,11 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {enableScreens} from 'react-native-screens';
 const Stack = createStackNavigator();
     <Stack.Navigator>
-      {/* Define your screens here */}
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="PreviousScreen" component={PreviousScreen} />
     </Stack.Navigator>
-  </React.Fragment>
   );
 };
+enableScreens();
 export default Navigation;
