1ae439924570f7a9790d21b17aeb125fd2efdd6f
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 02:20:58 -0400

    fix: apply solution for issue #75905

--- web/src/navigation/Navigation.js
+++ web/src/navigation/Navigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {enableScreens} from 'react-native-screens';
-const Stack = createStackNavigator();
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
-      <Stack.Screen name="PreviousScreen" component={PreviousScreen} />
-    </Stack.Navigator>
-  );
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+const Navigation = () => {
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Chat" component={ChatScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+    );
 };
-enableScreens();
+
 export default Navigation;
