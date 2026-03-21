f56832c82349e3531f671656c928c36c90967f81
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 06:03:07 -0400

    fix: apply solution for issue #75905

---
+++ web/src/components/ScreenWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {enableFreeze} from 'react-native-screens';
+const ScreenWrapper = ({children}) => {
+  return (
+    </SafeAreaView>
+  );
+};
+enableFreeze(true);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
