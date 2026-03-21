407e63cf5eb48fdab96fb81a33352002c2615a58
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 02:13:07 -0400

    fix: apply solution for issue #79389

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -17,7 +17,7 @@ function Inbox({isLoadingReportData}) {
         if (!SessionUtils.isAnonymousUser()) {
     return (
         <View style={[styles.flex1]}>
-            {isLoadingReportData && <SkeletonView />}
+            {isLoadingReportData && <ActivityIndicator size="large" color={styles.spinner.color} />}
             {!isLoadingReportData && <ReportList />}
         </View>
     );
@@ -26,9 +26,3 @@ Inbox.defaultProps = defaultProps;
 
 export default withOnyx({
     isLoadingReportData: {
-        key: ONYXKEYS.IS_LOADING_REPORT_DATA,
-    },
-    isLoadingReportData: {
-        key: ONYXKEYS.IS_LOADING_REPORT_DATA,
-    },
-})(Inbox);
