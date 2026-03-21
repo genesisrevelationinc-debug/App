86251649f149c0ba023e6047aacd6393e1737d1d
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 02:07:13 -0400

    fix: apply solution for issue #79389

--- src/libs/SessionUtils.js
+++ src/libs/SessionUtils.js
@@ -1,18 +1,17 @@
-import _ from 'underscore';
-import ONYXKEYS from '../ONYXKEYS';
-import * as API from '../API';
-import * as ReportActions from '../actions/Report';
-
-/**
- * Check if the user is an anonymous user.
-    return !session.authToken;
-}
-
-function fetchReportsOnLogin() {
-    ReportActions.fetchAllReports();
-}
-
-export {
-    isAnonymousUser,
-    fetchReportsOnLogin,
-};
+import lodashHas from 'lodash/has';
+import lodashIsEqual from 'lodash/isEqual';
+import lodashIsString from 'lodash/isString';
+import * as ReportActions from './actions/Report';
+import * as UserUtils from './UserUtils';
+import * as API from './API';
+import * as OnyxUtils from './OnyxUtils';
+        .then((response) => {
+            if (response.jsonCode === 200) {
+                Onyx.merge(ONYXKEYS.SESSION, {authToken: response.authToken});
+                // Ensure the Inbox is refreshed after successful login
+                Onyx.set(ONYXKEYS.IS_LOADING_REPORT_DATA, true);
+                ReportActions.fetchAllReports();
+                Onyx.set(ONYXKEYS.IS_LOADING_REPORT_DATA, false);
+                Navigation.dismissModal();
+            } else {
+                Onyx.merge(ONYXKEYS.SESSION, {error: response.message});
