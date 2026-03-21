ad5edeb66d748698433b93ee28eb6e076d07ce27
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 05:56:25 -0400

    fix: apply solution for issue #79389

--- src/libs/actions/Session.js
+++ src/libs/actions/Session.js
@@ -3,8 +3,8 @@
                 Onyx.merge(ONYXKEYS.SESSION, {authToken: response.authToken});
                 // Ensure the Inbox is refreshed after successful login
                 Onyx.set(ONYXKEYS.IS_LOADING_REPORT_DATA, true);
-                ReportActions.fetchAllReports();
+                Report.fetchAllReports();
                 Onyx.set(ONYXKEYS.IS_LOADING_REPORT_DATA, false);
-                Navigation.dismissModal();
+                Navigation.navigate(ROUTES.HOME);
             } else {
                 Onyx.merge(ONYXKEYS.SESSION, {error: response.message});
