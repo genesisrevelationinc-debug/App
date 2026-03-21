7b47517f157a28e35da0bbd35de3ce9c91be41c6
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 20:18:08 -0400

    fix: apply solution for issue #75905

---
+++ web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {enableScreens} from 'react-native-screens';
+enableScreens();
+ReactDOM.render(<App />, document.getElementById('root'));
