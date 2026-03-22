36fcb4d6eef114d699eaeff812329742003c454c
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-22 03:39:41 -0400

    fix: apply solution for issue #85557

--- src/components/MessageComposer.js
+++ src/components/MessageComposer.js
@@ -1,19 +1,22 @@
-import React, {useState} from 'react';
+import lodashGet from 'lodash/get';
+import lodashIsEqual from 'lodash/isEqual';
+import lodashMerge from 'lodash/merge';
 import {cacheAttachment} from '../libs/AttachmentUtils';
-import {sendChatMessage} from '../libs/ChatUtils';
-import TextInput from './TextInput';
 
-        sendChatMessage(chatId, message);
-        setMessage('');
+const propTypes = {
+    /** The report currently being looked at */
+            return;
+        }
 
         // Check for markdown image URLs and cache them
         const markdownImageRegex = /!\[.*?\]\((https?:\/\/.*?)\)/g;
         let match;
-        while ((match = markdownImageRegex.exec(message)) !== null) {
+        while ((match = markdownImageRegex.exec(messageText))) {
             fetch(match[1])
                 .then(response => response.blob())
                 .then(blob => cacheAttachment(match[1], blob));
         }
-    };
 
-    return (
+        // If we are editing a comment, we want to update it instead of creating a new one
+        if (this.state.isEditing) {
+            this.updateComment();
