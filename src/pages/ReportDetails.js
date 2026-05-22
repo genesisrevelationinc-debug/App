da28b04e92b3373bd59f49f3337938c100c0b810
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-22 18:05:04 -0400

    fix: apply solution for issue #91345

---
+++ src/pages/ReportDetails.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Button} from 'react-native';
+import { connect } from 'react-redux';
+import { getComponentToRender, getComponentToRender } from 'some-module';
+import { withNetwork } from 'some-module';
