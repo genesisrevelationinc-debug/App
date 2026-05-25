2e6c756e53bfb36cdc8608c1f8a7ab39f1e508f3
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-25 07:22:52 -0400

    fix: apply solution for issue #91502

--- src/components/DatePicker/DatePickerModal.tsx
+++ src/components/DatePicker/DatePickerModal.tsx
@@ -1,18 +1,21 @@
-import {setYear} from 'date-fns';
-import React, {useEffect, useRef, useState} from 'react';
-import type {View} from 'react-native';
-import PopoverWithMeasuredContent from '@components/PopoverWithMeasuredContent';
-import useResponsiveLayout from '@hooks/useResponsiveLayout';
-import useThemeStyles from '@hooks/useThemeStyles';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {View} from 'react-native';
+import type {StyleProp, ViewStyle} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
+import DatePicker from '@components/DatePicker';
+import FormHelpMessage from '@components/FormHelpMessage';
+import Modal from '@components/Modal';
 import {setDraftValues} from '@userActions/FormActions';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
-import CalendarPicker from './CalendarPicker';
-import type {DatePickerProps} from './types';
-
-const DEFAULT_ANCHOR_ORIGIN = {
+import Accessibility from '@libs/Accessibility';
+type DatePickerModalProps = {
+    /** Whether the modal is visible */
     horizontal: CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL.RIGHT,
     vertical: CONST.MODAL.ANCHOR_ORIGIN_VERTICAL.TOP,
 };
+
 const popoverDimensions = {
     height: CONST.POPOVER_DATE_MIN_HEIGHT,
     width: CONST.POPOVER_DATE_WIDTH,
@@ -31,26 +34,36 @@ function DatePickerModal({
     isVisible,
     onClose,
     anchorPosition,
+    anchorAlignment = DEFAULT_ANCHOR_ORIGIN,
     onSelected,
     shouldCloseWhenBrowserNavigationChanged = false,
     shouldPositionFromTop = false,
     forwardedFSClass,
+    shouldEnableMonthYearBackdropInNarrowPane = false,
 }: DatePickerProps) {
     const [selectedDate, setSelectedDate] = useState(value ?? defaultValue ?? undefined);
     const anchorRef = useRef<View>(null);
     const styles = useThemeStyles();
-
-    // We need to use isSmallScreenWidth instead of shouldUseNarrowLayout to distinguish RHL and narrow layout
-    // eslint-disable-next-line rulesdir/prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth
-    const {isSmallScreenWidth} = useResponsiveLayout();
-
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+    const datePickerRef = useRef<DatePicker>(null);
+    const isFocused = useIsFocused();
+    const [selectedDate, setSelectedDate] = useState<Date | string>(value ?? new Date());
+    const [errorMessage, setErrorMessage] = useState('');
     useEffect(() => {
         if (shouldSaveDraft && formID) {
-            setDraftValues(formID, {[inputID]: selectedDate});
+    useEffect(() => {
+        setSelectedDate(value ?? new Date());
+    }, [value]);
+
+    useEffect(() => {
+        if (isVisible && isFocused) {
+            Accessibility.announceForAccessibility(translate('common.calendarOpened'));
         }
-        if (selectedDate !== value) {
-            setSelectedDate(value);
-        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isVisible, isFocused]);
+    const handleDateChange = useCallback(
+        (date: Date) => {
     }, [formID, inputID, selectedDate, shouldSaveDraft, value]);
 
     const handleDateSelection = (newValue: string) => {
@@ -69,10 +82,10 @@ function DatePickerModal({
             popoverDimensions={popoverDimensions}
             shouldCloseWhenBrowserNavigationChanged={shouldCloseWhenBrowserNavigationChanged}
             innerContainerStyle={isSmallScreenWidth ? styles.w100 : {width: CONST.POPOVER_DATE_WIDTH}}
-            anchorAlignment={DEFAULT_ANCHOR_ORIGIN}
+            anchorAlignment={anchorAlignment}
             restoreFocusType={CONST.MODAL.RESTORE_FOCUS_TYPE.DELETE}
             shouldSwitchPositionIfOverflow
-            shouldEnableNewFocusManagement
+            shouldReturnFocus={false}
             shouldMeasureAnchorPositionFromTop={shouldPositionFromTop}
             shouldSkipRemeasurement
             forwardedFSClass={forwardedFSClass}
@@ -83,6 +96,7 @@ function DatePickerModal({
                 maxDate={maxDate}
                 value={selectedDate}
                 onSelected={handleDateSelection}
+                shouldEnableMonthYearBackdropInNarrowPane={shouldEnableMonthYearBackdropInNarrowPane}
             />
         </PopoverWithMeasuredContent>
     );
