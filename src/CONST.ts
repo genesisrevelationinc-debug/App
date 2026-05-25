56072f47919b1751f4dcc09faa25e43caedc7d53
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-25 04:40:22 -0400

    fix: apply solution for issue #91300

---
+++ src/CONST.ts
@@ -0,0 +1,25 @@
+    BANK_ACCOUNT_STATUS: {
+        VERIFIED: 'VERIFIED',
+        SETUP: 'SETUP',
+        PENDING: 'PENDING',
+        VERIFYING: 'VERIFYING',
+        LOCKED: 'LOCKED',
+    },
+    
+    BANK_ACCOUNT_DISPLAY_STATUS: {
+        VERIFIED: 'Active',
+        SETUP: 'Incomplete',
+        PENDING: 'Pending',
+        VERIFYING: 'Verifying',
+        LOCKED: 'Locked',
+    },
+    
+    CARD_STATUS: {
+        ACTIVE: 'ACTIVE',
+        INACTIVE: 'INACTIVE',
+    },
+    
+    CARD_DISPLAY_STATUS: {
+        ACTIVE: 'Active',
+        INACTIVE: 'Inactive',
+    },
