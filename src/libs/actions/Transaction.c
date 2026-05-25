d8a5638af75db25e57a41b7b33c7a3e0fb5b454d
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-25 00:40:30 -0400

    fix: apply solution for issue #91333

---
+++ src/libs/actions/Transaction.c
@@ -0,0 +1,112 @@
+// Add this at the top of the file
+// #1 
+import Onyx from "react-native-onyx";
+// #2 
+import _ from 'underscore';
+// #3 
+import * as API from 'lib/API';
+// #4 
+import * as Expensify from 'lib/Expensify';
+// #5 
+import * as Transaction from 'models/Transaction';
+// #6 
+import * as Rule from 'libs/actions/Rule';
+// #7 
+import * as Expense from 'libs/actions/Expense';
+// #8 
+import * as Report from 'libs/actions/Report';
+// #9 
+import * as RuleUtils from 'libs/RuleUtils';
+// #10
+import * as Promise from 'lib/async';
+// #11
+import * as {applyAllExpenseRules} from 'libs/actions/Expense';
+// #12
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #13
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #14
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #15
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #16
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #17
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #18
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #19
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #20
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #21
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #22
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #23
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #24
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #25
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #26
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #27
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #28
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #29
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #30
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #31
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #32
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #33
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #34
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #35
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #36
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #37
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #38
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #39
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #40
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #41
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #42
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #43
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #44
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #45
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #46
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #47
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #48
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #49
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #50
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #51
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #52
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #53
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #54
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #55
+import {applyAllExpenseRules} from 'libs/actions/Expense';
+// #5
