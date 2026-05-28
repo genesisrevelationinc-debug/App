61cfed78d3ef1f0500d1c43d6f2a84f276c045e6
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-27 20:01:35 -0400

    fix: apply solution for issue #91006

---
+++ src/components/ReportActionItem/MoneyRequestView.js
@@ -0,0 +1,7 @@
+    const currency = lodashGet(props, 'action.originalMessage.currency', CONST.CURRENCY.USD);
+    const isSettled = lodashGet(props, 'action.originalMessage.isSettled', false);
+    
+    const displayAmount = (amount || amount === 0) ? CurrencyUtils.convertToDisplayString(amount, currency) : '';
+    
+    // If the money request is deleted, don't show the details
+    if (lodashGet(props, 'action.originalMessage.type') === 'delete') {
