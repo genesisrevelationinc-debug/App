a6ca6c4dbcfeadbd2bc8b9e368d0d7820aace7e6
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-28 12:14:41 -0400

    fix: apply solution for issue #90729

---
+++ src/libs/Navigation/NavigationLinking.js
@@ -0,0 +1,17 @@
+    const navigationRef = createRef();
+
+    return {
+        // eslint-disable-next-line no-useless-concat
+        if (Platform.OS === 'ios' && !options.create) {
+            return;
+        }
+        if (options.create) {
+            return;
+        }
+        const finalURL = buildAPIURL(name, options);
+        if (options.simplify)+ {
+    }
+};
+
+const linking = {
+    // in-exp
