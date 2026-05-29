fafb085c1193e6b88bdead1d95db962875d16c40
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 12:20:16 -0400

    fix: apply solution for issue #91927

--- src/components/Composer/index.tsx
+++ src/components/Composer/index.tsx
@@ -1,24 +1,16 @@
 import type {MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
-import React, {useCallback, useEffect, useMemo, useRef, useState, useLayoutEffect} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import type {NativeSyntheticEvent, TextInput, TextInputSelectionChangeEventData} from 'react-native';
 import {useSharedValue} from 'react-native-reanimated';
     const [isRendered, setIsRendered] = useState(false);
     const [isFocused, setIsFocused] = useState(false);
     const [isInitialized, setIsInitialized] = useState(false);
-    const [textInputHeight, setTextInputHeight] = useState(0);
     const textInput = useRef<TextInput | null>(null);
     const isIOS = getPlatform() === 'ios';
     const isWeb = getPlatform() === 'web';
         [styles, maxLines, isComposerFullScreen],
     );
-
-    useLayoutEffect(() => {
-        if (!textInput.current) {
-            return;
-        }
-        textInput.current.measure((x, y, width, height) => setTextInputHeight(height));
-    }, [textInput, isComposerFullScreen, value]);
 
     const maxHeight = useMemo(() => {
         if (isComposerFullScreen) {
@@ -26,14 +18,7 @@ import {useSharedValue} from 'react-native-reanimated';
     const composerStyle = useMemo(
         () => ({
             ...styles.textInput,
-            ...(isComposerFullScreen ? {minHeight: textInputHeight} : {}),
+            ...(isComposerFullScreen ? styles.composerHeightFullScreen : {}),
             maxHeight,
             ...style,
         }),
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [maxHeight, isComposerFullScreen, style, styles.textInput, styles.composerHeightFullScreen],
-    );
-
-    const handleSelectionChange = useCallback(
-        (e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
-            if (!onSelectionChange) {
