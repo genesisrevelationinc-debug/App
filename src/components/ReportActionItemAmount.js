30335591c0faa35e7af45a3e417326e89762a9d6
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 01:51:10 -0400

    fix: apply solution for issue #91749

--- src/components/ReportActionItemAmount.js
+++ src/components/ReportActionItemAmount.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import _ from 'underscore';
 import PropTypes from 'prop-types';
 import styles from '../styles/styles';
 import {withNetwork} from '../components/OnyxProvider';
-import * as ReportUtils from '../libs/ReportUtils';
 import * as Localize from '../libs/Localize';
-import * as SplitUtils from '../libs/SplitUtils';
+import * as ReportUtils from '../libs/ReportUtils';
 
+/**
+ * Show the amount with a split indicator if the expense has splits
+ */
 const ReportActionItemAmount = (props) => {
-    const hasRemovedSplits = SplitUtils.hasRemovedSplits(props.reportID);
+    const hasSplits = ReportUtils.hasRemovedSplitAmounts(props.reportID, props.action);
     
     return (
         <View style={[styles.amount, styles.alignSelfCenter]}>
             <Text style={[styles.amount, styles.alignSelfCenter]}>
                 {Localize.numberFormat(props.action.amount, {precision: 2})}
             </Text>
-            {hasRemovedSplits && (
+            {hasSplits && (
                 <View style={styles.splitDot}>
                     <Text style={styles.splitDotText}>•</Text>
                 </View>
