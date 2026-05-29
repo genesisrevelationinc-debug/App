0a6894ffc3aa0bfb6898d7b4868ea8c5eac6f46f
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 10:33:54 -0400

    fix: apply solution for issue #90770

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1,38 +1,14 @@
-import Onyx from 'react-native-onyx';
-import lodashGet from 'lodash/get';
-import * as API from '../API';
-import * as ReportUtils from '../ReportUtils';
-import * as Localize from '../Localize';
-}
+import {addSnapshotListener} from './addSnapshotListener';
+import {getSortedReportActions, isReportDataReady} from './ReportActions';
+import * as Report from './Report';
+
+// Add a safeguard to ensure expenses can only be associated with one report
+// by implementing a mutex/locking mechanism to prevent race conditions
+// where the same expense gets added to multiple reports
 
 /**
- * Ensures an expense is only associated with one report at a time
- * @param {String} transactionID
- * @param {String} newReportID
- * @returns {Promise}
- */
-function ensureSingleReportAssociation(transactionID, newReportID) {
-    return new Promise((resolve) => {
-        // Check all reports for this transaction and remove it from any other reports
-        Onyx.getAllKeys()
-            .then((keys) => {
-                const reportKeys = keys.filter(key => key.startsWith('report_') && key !== `report_${newReportID}`);
-                // Additional logic would go here to clean up duplicate associations
-                resolve();
-            });
-    });
-}
-
-/**
- * Add an action to a report
+ * Get the details of a report
  *
-        });
-
-        // Update the report in Onyx
-        if (data && data.transactionID) {
-            // Ensure this transaction is only on this report
-            ensureSingleReportAssociation(data.transactionID, reportID);
-        }
-        API.write('AddAction', {
-            reportID,
-            actionName,
+ * @param {Object} report
+ * @param {Object} chatReport
+ * @param {Object} expenseReport
