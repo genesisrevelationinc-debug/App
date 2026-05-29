ad9e9204aa1c49db1f83f044e431e4c31ee42b4d
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 01:51:06 -0400

    fix: apply solution for issue #91749

--- src/components/ReportActionItemAmount.js
+++ src/components/ReportActionItemAmount.js
@@ -1,17 +1,25 @@
-   // Check if this expense action has splits that were removed
-   const hasRemovedSplits = ReportUtils.hasRemovedSplitAmounts(reportID, action);
-   
-   return (
-       <View style={[styles.amount, styles.alignSelfCenter]}>
-           <Text style={[styles.amount, styles.alignSelfCenter]}>
-               {Localize.numberFormat(action.amount, {precision: 2})}
-           </Text>
-           {hasRemovedSplits && (
-               <View style={styles.splitDot}>
-                   <Text style={styles.splitDotText}>•</Text>
-               </View>
-           )}
-       </View>
-   );
-   action: PropTypes.object.isRequired,
-   reportID: PropTypes.string.isRequired,
+import React from 'react';
+import {View, Text} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../styles/styles';
+import {withNetwork} from '../components/OnyxProvider';
+import * as ReportUtils from '../libs/ReportUtils';
+import * as Localize from '../libs/Localize';
+import * as SplitUtils from '../libs/SplitUtils';
+
+const ReportActionItemAmount = (props) => {
+    const hasRemovedSplits = SplitUtils.hasRemovedSplits(props.reportID);
+    
+    return (
+        <View style={[styles.amount, styles.alignSelfCenter]}>
+            <Text style={[styles.amount, styles.alignSelfCenter]}>
+                {Localize.numberFormat(props.action.amount, {precision: 2})}
+            </Text>
+            {hasRemovedSplits && (
+                <View style={styles.splitDot}>
+                    <Text style={styles.splitDotText}>•</Text>
+                </View>
+            )}
+        </View>
+    );
+};
