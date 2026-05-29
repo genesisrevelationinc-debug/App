efe386d43cf60e264090791369596043d9cb1323
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 11:03:43 -0400

    fix: apply solution for issue #91919

--- src/pages/home/report/ReportActionsView.js
+++ src/pages/home/report/ReportActionsView.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React, {useRef, useEffect, useCallback, useMemo} from 'react';
+import {useIsFocused, useFocusEffect} from '@react-navigation/native';
+import {View, InteractionManager} from 'react-native';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
 import _ from 'underscore';
-import styles from 'styles/styles';
-import * as Report from 'Report';
-import * as ReportActions from 'ReportActions';
-import * as ReportUtils from 'ReportUtils';
-import * as ReportScrollManager from 'ReportScrollManager';
-import withWindowDimensions from 'components/withWindowDimensions';
-import withLocalize from 'components/withLocalize';
-import withNetwork from 'components/withNetwork';
-import withPersonalDetails from 'components/withPersonalDetails';
-import withCurrentUserPersonalDetails from 'components/withCurrentUserPersonalDetails';
-import withOnyx from 'react-native-onyx';
-import ONYXKEYS from 'Onyx';
-import compose from 'lib/compose';
-import * as ReportActionCompose from 'components/ReportActionCompose';
-import * as ReportActionsView from 'components/ReportActionsView';
-import * as ReportActionItem from 'components/ReportActionItem';
-import * as ReportActionsUtils from 'ReportActionsUtils';
-import * as ReportUtils from 'ReportUtils';
-import * as Report from 'Report';
-import * as OptionsListUtils from 'OptionsListUtils';
-import * as PersonalDetailsUtils from 'PersonalDetailsUtils';
-import * as UserUtils from 'UserUtils';
-import * as Link from 'Link';
-import * as Environment from 'Environment';
-import * as Policy from 'Policy';
-import * as ReportActions from 'ReportActions';
-import * as ReportActionsUtils from 'ReportActionsUtils';
-import * as ReportUtils from 'ReportUtils';
-import * as ReportScrollManager from 'ReportScrollManager';
-import * as ReportActionCompose from 'components/ReportActionCompose';
-import * as ReportActionsView from 'components/ReportActionsView';
-import * as ReportActionItem from 'components/ReportActionItem';
-import * as Report from 'Report';
-import * as ReportUtils from 'ReportUtils';
-import * as ReportActions from 'ReportActions';
-import * as ReportActionsUtils from 'ReportActionsUtils';
+    const prevReportActionsRef = useRef(reportActions);
+    const prevReportRef = useRef(props.report);
+    const prevIsSmallScreenWidthRef = useRef(isSmallScreenWidth);
+    const lastActionRef = useRef(null);
+
+    const isFocused = useIsFocused();
+    const isReportFullyVisible = isFocused && !props.isComposerFullSize;
+        }
+    }, [props.report, prevReportRef, isFocused, isAtBottom, isReportFullyVisible]);
+
+    // Scroll to bottom when a new action is added
+    useFocusEffect(
+        useCallback(() => {
+            // Check if a new action was added
+            const currentActions = ReportActionsUtils.getSortedReportActionsForDisplay(props.report.reportID);
+            if (currentActions.length > 0) {
+                const lastAction = currentActions[currentActions.length - 1];
+                if (lastActionRef.current && lastActionRef.current !== lastAction.reportActionID) {
+                    scrollToBottom();
+                }
+                lastActionRef.current = lastAction ? lastAction.reportActionID : null;
+            }
+        }, [props.report.reportID, scrollToBottom])
+    );
+
+    useEffect(() => {
+        const prevReport = prevReportRef.current;
+        const prevReportActions = prevReportActionsRef.current;
+
+        // When the user moves their focus to the Composer, we want to check if the report is fully visible
+        // If the report is fully visible, we want to scroll the list to the bottom
+        const currentActions = ReportActionsUtils.getSortedReportActionsForDisplay(props.report.reportID);
+        let shouldScrollToBottom = false;
+
+        // Always scroll to bottom for new actions, regardless of paid expense status
+        if (currentActions.length > 0) {
+            const lastAction = currentActions[currentActions.length - 1];
+            if (lastActionRef.current !== (lastAction ? lastAction.reportActionID : null)) {
+                shouldScrollToBottom = true;
+                lastActionRef.current = lastAction ? lastAction.reportActionID : null;
+            }
+        }
+
+        if (!isReportFullyVisible || !isAtBottom) {
+            return;
+        }
+            return;
+        }
+
+        // Always scroll to bottom for new actions, even with paid expenses
+        if (shouldShowLoader && !shouldScrollToBottom) {
+            return;
+        }
+
+
+        scrollToBottom();
+    }, [props.report, prevReportRef, isAtBottom, isReportFullyVisible, prevIsSmallScreenWidthRef, shouldShowLoader, scrollToBottom]);
