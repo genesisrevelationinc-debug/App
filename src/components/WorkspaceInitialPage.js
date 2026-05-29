4b14944241ec49a5a259ddfeb4d59037249cc715
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-05-29 09:47:49 -0400

    fix: apply solution for issue #88977

---
+++ src/components/WorkspaceInitialPage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {View} 
+from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../styles/themes';
+import withWindowDimensions from './withWindowDimensions';
+import withLocalize from './withLocalize';
+import * as Expensicons from './Icon/Expensicons';
+import * as Illustrations from './Illustrations';
+import * as UserUtils from '../libs/UserUtils';
+import * as Policy from '../libs/actions/Policy';
+import * as PolicyUtils from '../libs/PolicyUtils';
+import * as Reig
+from './Navigation/Navigation';
+import * as OptionsListUtils from '../libs/OptionsListUtils';
+import * as ReportUtils from '../libs/ReportUtils';
+import * as StyleUtils from '../libs/StyleUtils';
+import * as DeviceCapabilities from '../libs/DeviceCapabilities';
+import * as Expens
+from '../';
+import * as ActiveClientManager from '../libs/ActiveClientManager';
+import * as Welcome from '../pages/workspace/WorkspaceInitialPage';
+import * as WelcomeLeftPane from '../pages/workspace/WorkspaceInitialPage';
+import * as WelcomeRightPane from '../pages/workspace/Welcome/WelcomePage';
+import * as WelcomeRightP
+from '../pages/workspace/WorkspacePage';
+import * as Work
+from '../';
+import * as Work
+from '../pages/workspace/WorkspaceInitialPage';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspacePageWithPanels';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspaceInitialPage';
+import * as Work
+from '../pages/workspace/WorkspacePage';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspacePage';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspacePage';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspacePage';
+import * as Work
+from '../pages/workspace/WorkspaceSettingsPage';
+import * as Work
+from '../pages/workspace/WorkspaceEditor';
+import * as Work
+from '../pages/workspace/WorkspacePage';
+import *
