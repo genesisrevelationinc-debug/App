e3cecd1cb4718e920bfdedd7f91f97e04a3f0fc4
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-06 09:46:55 -0400

    fix: apply solution for issue #92764

--- src/pages/workspace/WorkspaceNewPage.tsx
+++ src/pages/workspace/WorkspaceNewPage.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {Keyboard, View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
 import type {ValueOf} from 'type-fest';
