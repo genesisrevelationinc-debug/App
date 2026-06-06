cf7a7325b088927017d1146dcef49f505f0ad4d2
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-06 09:55:20 -0400

    fix: apply solution for issue #91448

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -1,69 +1,14 @@
-import lodashGet from 'lodash/get';
-import * as ReportActions from './actions/ReportActions';
-import * as CollectionUtils from './CollectionUtils';
-import * as ErrorUtils from './ErrorUtils';
-import * as LocalePhoneNumber from './LocalePhoneNumber';
-import * as ReportActionsUtils from './ReportActionsUtils';
-import * as ReceiptUtils from './ReceiptUtils';
-import * as FileUtils from './FileUtils';
-import * as Link from './Navigation/Link';
-import * as OptionsListUtils from './OptionsListUtils';
-import * as CollectionDataSetup from './components/OnyxProviderUtil';
-
-import * as PersonalDetailsUtils from './PersonalDetailsUtils';
-import * as RoomNotifPreference from './RoomNotifPreference';
-import * as Localize from './Localize';
-import * as Navigation from './Navigation';
-
-// eslint-disable-next-line import/no-cycle
-import * as Report from './Report';
 import * as UserUtils from './UserUtils';
-import * as Task from './actions/Task';
-import * as TransactionUtils from './TransactionUtils';
-import * as ScrollUtils from './libs/ScrollUtils';
-
-/**
- * Returns the concatenated title for the policy rooms
-    }
-}
-
-/**
- * @param {String} url
- * @returns {Boolean}
- */
-function handleInAppScrollToMessage(url) {
-    if (!url.includes('concierge')) {
-        return false;
-    }
-    
-    const reportID = url.match(/report\/(\d+)/);
-    if (!reportID) return false;
-    
-    return true;
-}
-
-/**
- * @param {String} url
- * @returns {String}
- */
-function getRoutePath(url) {
-    const path = url.match(/\/r\/(\d+)\/(.+?)\/(.+?)(?:_.*?)?/);
-    return path ? `/r/${path[1]}/${path[2]}/${path[3]}` : '';
-}
-
-/**
- */
-function openReportFromDeepLink(url) {
-    const route = getRoutePath(url);
-    if (!handleInAppScrollToMessage(url)) {
-    const json = JSON.parse(decodeURI(route));
-        }
-    }
-
-    if (json.reportID) {
-        Navigation.navigate(route, {reportID: json.reportID, chatReportID: json.reportID});
-    }
-}
-}
-
-/**
+import * as CachingUtils from './CachingUtils';
+import * as User from './User';
+import * as Link from './Link';
+import * as Localize from './Localize';
+import * as Locale from './Locale';
+import * as Pusher from './Pusher';
+import * as Welcome from './Welcome';
+import * as OptionsListUtils from './OptionsListUtils';
+import * as ReportActionsUtils from './ReportActionsUtils';
+import * as ReportScrollManager from './ReportScrollManager';
+import * as EmojiUtils from './EmojiUtils';
+import * as PersonalDetails from './PersonalDetails';
+import * as UserLocal from './User';
