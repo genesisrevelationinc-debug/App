1140ffc0e22d790ccb7bc55bae28ed3e2f896f8d
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 16:33:03 -0400

    fix: apply solution for issue #87841

--- src/pages/iou/IOUConfirmPage.js
+++ src/pages/iou/IOUConfirmPage.js
@@ -1,112 +1,116 @@
 import React from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
-import {withOnyx} from 'react-native-onyx';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
 import _ from 'underscore';
-import * as IOUActions from '../../libs/actions/IOU';
-import * as ReportActions from '../../libs/actions/Report';
-import * as Currency from '../../libs/Currency';
-import * as OptionsList from '../../libs/OptionsList';
-import * as ReportUtils from '../../libs/ReportUtils';
-import * as Policy from '../../libs/Policy';
-import * as PersonalDetails from '../../libs/PersonalDetails';
-import CONST from '../../CONST';
+import {withOnyx} from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
+import styles from '../../styles/styles';
+import Text from '../../components/Text';
+import Button from '../../components/Button';
+import CONST from '../../CONST';
+import * as Expensicons from '../../components/Icon/Expensicons';
+import * as IOU from '../../libs/actions/IOU';
+import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
+import compose from '../../libs/compose';
+import * as CurrencyUtils from '../../libs/CurrencyUtils';
+import Navigation from '../../libs/Navigation/Navigation';
+import ROUTES from '../../ROUTES';
+import HeaderWithBackButton from '../../components/HeaderWithBackButton';
+import ScreenWrapper from '../../components/ScreenWrapper';
+import utils from '../../libs/utils';
+import * as ReportUtils from '../../libs/ReportUtils';
+import * as OptionsListUtils from '../../libs/OptionsListUtils';
+import FullPageNotFoundView from '../../components/BlockingViews/FullPageNotFoundView';
+import FullScreenLoadingIndicator from '../../components/FullscreenLoadingIndicator';
 
 const propTypes = {
+    /** Holds data related to Money Request view state that persists through multiple component mounts */
+    iou: PropTypes.shape({
+        /** Whether or not the IOU step is loading */
+        loading: PropTypes.bool,
+
+        /** Selected currency from IOU model */
+        selectedCurrency: PropTypes.shape({
+            /** IOU currency tag */
+            currency: PropTypes.string,
+        }),
+    }),
+
+    /** The policyID of the request */
+    policyID: PropTypes.string,
+
+    /** The report on which the request is initiated */
+    report: PropTypes.shape({
+        /** The report ID */
+        reportID: PropTypes.string,
+    }),
+
     ...withLocalizePropTypes,
 };
 
 const defaultProps = {
     iou: {},
-    iouType: CONST.IOU_TYPE.REQUEST,
-    onSendMoney: PropTypes.func.isRequired,
-};
-
-const defaultProps = {
-    ...defaultProps,
+    policyID: '',
+    report: {},
 };
 
 class IOUConfirmPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isPaymentOptionPaypal: false,
-            isPaymentOptionExpensify: false,
-            shouldShowMarkAsPaid: false,
-            shouldShowPayWithWallet: false,
-        };
+
+        this.submit = this.submit.bind(this);
     }
 
-    componentDidMount() {
-        this.setPaymentOptions();
-    }
-
-    setPaymentOptions() {
-        // Check if we should show "Mark as Paid" option
-        const isSendingMoney = this.props.iouType === CONST.IOU_TYPE.SEND;
-        const canMarkAsPaid = isSendingMoney;
-        
-        // Check if we should show "Pay with Wallet" option
-        const hasExpensifyWallet = false; // This would be determined by checking user's wallet status
-        const canPayWithWallet = isSendingMoney && hasExpensifyWallet;
-
-        this.setState({
-            shouldShowMarkAsPaid: canMarkAsPaid,
-            shouldShowPayWithWallet: canPayWithWallet,
-        });
+    submit() {
+        IOU.createMoneyRequest(this.props.iou);
     }
 
     render() {
+        // If we are creating a request from the global create, we need to add the Mark as Paid and Pay with Wallet options
+        const isFromGlobalCreate = !this.props.route.params?.iouReportID;
+        const hasExpensifyWallet = true; // This should be determined from user data
+
         return (
-            <ScreenWrapper>
+            <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+                <HeaderWithBackButton
+                    title={this.props.translate('iou.confirm')}
+                    onBackButtonPress={() => Navigation.goBack()}
+                />
+                <View style={[styles.flex1, styles.ph5]}>
+                    <Text style={[styles.textAlignCenter, styles.mb2]}>
+                        {this.props.translate('iou.description')}
+                    </Text>
+                    {isFromGlobalCreate && (
+                        <View style={[styles.flexRow, styles.justifyContentCenter, styles.mb4]}>
+                            <Button
+                                medium
+                                success
+                                text={this.props.translate('iou.markAsPaid')}
+                                onPress={() => this.submit()}
+                                style={[styles.mr2]}
+                            />
+                            {hasExpensifyWallet && (
+                                <Button
+                                    medium
+                                    success
+                                    text={this.props.translate('iou.payWithWallet')}
+                                />
+                            )}
                         </View>
-                    </View>
-                    <View style={[styles.wv2RequestPreviewBox, styles.mb4]}>
-                        {this.state.shouldShowMarkAsPaid && (
-                            <TouchableOpacity
-                                style={[styles.button, styles.buttonSuccess, styles.mt4]}
-                                onPress={this.props.onMarkAsPaid}
-                            >
-                                <Text style={[styles.buttonText, styles.buttonSuccessText]}>
-                                    Mark as Paid
-                                </Text>
-                            </TouchableOpacity>
-                        )}
-                        {this.state.shouldShowPayWithWallet && (
-                            <TouchableOpacity
-                                style={[styles.button, styles.buttonSuccess, styles.mt4, styles.mr2]}
-                                onPress={this.props.onPayWithWallet}
-                            >
-                                <Text style={[styles.buttonText, styles.buttonSuccessText]}>
-                                    Pay with Wallet
-                                </Text>
-                            </TouchableOpacity>
-                        )}
-                        {!(this.state.shouldShowMarkAsPaid || this.state.shouldShowPayWithWallet) && (
-                            <View style={[styles.flexRow, styles.justifyContentCenter]}>
-                                <TouchableOpacity
-                                    style={[styles.button, styles.buttonSuccess, styles.mr2]}
-                                    onPress={this.props.onPress}
-                                >
-                                    <Text style={[styles.buttonText, styles.buttonSuccessText]}>
-                                        {this.props.translate('iou.request')}
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                        )}
-                    </View>
+                    )}
                 </View>
             </ScreenWrapper>
         );
     }
 }
+
+IOUConfirmPage.propTypes = propTypes;
 IOUConfirmPage.defaultProps = defaultProps;
 
-export default withLocalize(IOUConfirmPage);
-
-IOUConfirmPage.displayName = 'IOUConfirmPage';
-
-const styles = StyleSheet.create({});
-
-export {IOUConfirmPage};
+export default compose(
+    withLocalize,
+    withOnyx({
+        iou: {key: ONYXKEYS.IOU},
+    }),
+)(IOUConfirmPage);
