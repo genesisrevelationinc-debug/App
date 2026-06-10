ebe6f88fdca76d6c3648281281fe0b26adf0da6f
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 16:18:55 -0400

    fix: apply solution for issue #91333

--- src/libs/actions/Transaction.js
+++ src/libs/actions/Transaction.js
@@ -1,36 +1,10 @@
-// Example of what needs to be updated (this is a common pattern for expense processing)
-
-/**
- * @param {String} transactionID
- * @param {Object} transaction
- * @returns {Promise}
- */
-function formatTransactionList(transaction) {
-    // Add the comment to the transaction object
-    return transaction;
-}
-
-/**
- * Process imported transaction and apply workspace rules
- * @param {Object} transaction
- * @returns {Promise}
- */
-function processImportedTransaction(transaction) {
-    // When transactions are imported via company card/bank feed, we need to ensure
-    // that workspace rules are applied automatically
-    return API.read('ApplyAllExpenseRules', {
-        transactionID: transaction.transactionID,
-    }, {
-        // Additional parameters if needed
-    });
-}
-
-/**
- * Main function that would be called when importing transactions
- * @param {Object} transaction
- * @returns {Object}
- */
-function processTransaction(transaction) {
-    // Process the transaction and apply rules
-    return transaction;
-}
+import Onyx from 'react-native-onyx';
+import ONYXKEYS from '../../ONYXKEYS';
+import * as API from '../API';
+import * as Report from './Report';
+import * as ReportActions from './ReportActions';
+import * as Policy from './Policy';
+import * as PersonalDetails from './PersonalDetails';
+import * as User from './User';
+import * as TransactionUtils from '../TransactionUtils';
+import * as Rule from './Rule';
