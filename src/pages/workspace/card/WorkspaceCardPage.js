795466bf2e2d8621d2cdeae812f0bd04687d76e1
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 17:49:44 -0400

    fix: apply solution for issue #92089

--- src/pages/workspace/card/WorkspaceCardPage.js
+++ src/pages/workspace/card/WorkspaceCardPage.js
@@ -1,48 +1,73 @@
-import React from 'react';
-import {View} from 'react-native';
-import Text from '../../../components/Text';
-import styles from '../../styles/styles';
-import CheckboxWithLabel from '../../../components/CheckboxWithLabel';
-import Datepicker from '../../../components/DatePicker';
-import * as Expensicons from '../../components/Icon/Expensicons';
+import React, {useState} from 'react';
+import {View} from 'components/View';
+import {withLocalize} from 'components/withLocalize';
+import {useNetwork} from 'components/NetworkContext';
+import {useTheme} from 'hooks/useTheme';
+import * as Card from 'components/Card';
+import * as ExpensiForm from 'components/Form';
+import * as ExpensiPicker from 'components/ExpensiPicker';
+import * as DatePicker from 'components/DatePicker';
+import * as Text from 'components/Text';
+import * as Button from 'components/Button';
+import * as assignCard from 'libs/actions/Card';
 
-class WorkspaceCardAssignPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isDateScheduled: false,
-            scheduledDate: '',
-        };
-    }
+const WorkspaceCardPage = () => {
+    const {translate} = withLocalize();
+    const {isOffline} = useNetwork();
+    const theme = useTheme();
+    const [assignee, setAssignee] = useState('');
+    const [assignmentDate, setAssignmentDate] = useState(new Date());
+    const [shouldSchedule, setShouldSchedule] = useState(false);
+    
+    const assigneeOptions = [
+        {label: 'Assign immediately', value: 'immediate'},
+        {label: 'Schedule for later', value: 'scheduled'}
+    ];
 
-    toggleScheduledAssignment = () => {
-        this.setState(prevState => ({isDateScheduled: !prevState.isDateScheduled}));
-    };
-
-    render() {
-        const {isDateScheduled, scheduledDate} = this.state;
-        
-        return (
-            <View style={[styles.mh5, styles.mb5]}>
-                <Text style={[styles.textLabel, styles.mb1]}>Schedule future assignment</Text>
-                <CheckboxWithLabel
-                    isChecked={isDateScheduled}
-                    onPress={this.toggleScheduledAssignment}
-                    style={[styles.mt4, styles.mb-12]}
-                    LabelComponent={() => <Text>Future Date Assignment</Text>}
+    return (
+        <View style={[theme.p5]}>
+            <Text style={[theme.textLarge, theme.mb3]}>Assign Card</Text>
+            
+            <ExpensiForm.default
+                onSubmit={() => {
+                    if (shouldSchedule) {
+                        assignCard.assignCardScheduled(card, assignee, assignmentDate);
+                    } else {
+                        assignCard.assignCardImmediate(card, assignee);
+                    }
+                }}
+            >
+                <ExpensiForm.Section
+                    title="Card Assignment"
+                    children={
+                        <View>
+                            <ExpensiPicker
+                                label={translate('Assign to')}
+                                items={assigneeOptions}
+                                value={assignee}
+                                onInputChange={(value) => setAssignee(value)}
+                            />
+                            
+                            <DatePicker
+                                label="Assignment Date"
+                                value={assignmentDate}
+                                onInputChange={setAssignmentDate}
+                            />
+                            
+                            <ExpensiPicker
+                                label="Assignment Type"
+                                items={[
+                                    {label: 'Assign now', value: 'immediate'},
+                                    {label: 'Schedule for future date', value: 'future'}
+                                ]}
+                                onInputChange={setShouldSchedule}
+                            />
+                        </View>
+                    }
                 />
-                {isDateScheduled && (
-                    <View style={[styles.mt3]}>
-                        <Text style={[styles.textLabel, styles.mb1]}>Assignment Date</Text>
-                        <Datepicker
-                            value={scheduledDate}
-                            onInputChange={(date) => this.setState({scheduledDate: date})}
-                        />
-                    </View>
-                )}
-            </View>
-        );
-    }
-}
+            </ExpensiForm.default>
+        </View>
+    );
+};
 
-export default WorkspaceCardAssignPage;
+export default withLocalize(WorkspaceCardPage);
