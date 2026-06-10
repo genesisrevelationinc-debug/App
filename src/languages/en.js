554c821f7fbd03ffc744b9be458c40324b370aaf
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-10 17:22:07 -0400

    fix: apply solution for issue #92089

---
+++ src/languages/en.js
@@ -0,0 +1,9 @@
+   workspace: {
+       companyCards: {
+           assignCard: 'Assign card',
+           assignDate: 'Assign date',
+           assignDateFuture: 'Assignment will take effect on the selected date',
+           effectiveDate: 'Effective date',
+           selectDate: 'Select date',
+       }
+   }
