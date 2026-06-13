91f29d535976def12d08015f32bde7a96e78c953
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-13 02:17:22 -0400

    fix: apply solution for issue #93455

--- src/libs/Notification/LocalNotification/index.web.ts
+++ src/libs/Notification/LocalNotification/index.web.ts
@@ -1,12 +1,21 @@
 import type {NotificationCommentParams, NotificationReportActionParams} from '@libs/Notification/types';
-import getNotificationIcon from '@libs/Notification/getNotificationIcon';
+
+function getNotificationIcon(): string | undefined {
+    // Edge browser requires a valid icon URL or undefined, but not an empty string
+    return undefined;
+}
 
 function createNotification(title: string, body: string, icon: string, onClick: () => void) {
     const notification = new Notification(title, {
+        body,
+        ...(icon ? {icon} : {}),
+    });
+    notification.onclick = () => {
+        window.parent.focus();
 }
 
 function showCommentNotification({title, body, onClick}: NotificationCommentParams) {
-    createNotification(title, body, getNotificationIcon(), onClick);
+    createNotification(title, body, getNotificationIcon() ?? '', onClick);
 }
 
 function showReportActionNotification({title, body, onClick}: NotificationReportActionParams) {
