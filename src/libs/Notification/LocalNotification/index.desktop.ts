aa1db3986adf0af3f3f039c5365268f6f9acf4b0
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-13 02:17:19 -0400

    fix: apply solution for issue #93455

---
+++ src/libs/Notification/LocalNotification/index.desktop.ts
@@ -0,0 +1,13 @@
+import {ipcRenderer} from 'electron';
+import type {NotificationCommentParams, NotificationReportActionParams} from '@libs/Notification/types';
+import {getNotificationIcon} from '@libs/Notification/NotificationIcon';
+
+function createNotification(title: string, body: string, icon: string, onClick: () => void) {
+    const notification = new Notification(title, {
+}
+
+function showCommentNotification({title, body, onClick}: NotificationCommentParams) {
+    createNotification(title, body, getNotificationIcon(), onClick);
+}
+
+function showReportActionNotification({title, body, onClick}: NotificationReportActionParams) {
