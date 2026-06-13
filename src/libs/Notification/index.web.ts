657750c210ef097518aa1372a9a99b16dc757ecd
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-13 02:10:40 -0400

    fix: apply solution for issue #93455

--- src/libs/Notification/index.web.ts
+++ src/libs/Notification/index.web.ts
@@ -1,14 +1,24 @@
 import {Str} from 'expensify-common';
 import type {ReportAction} from '@src/types/onyx';
+import * as Browser from '@libs/Browser';
 import type {NotificationComment} from './types';
-import Browser from '@libs/Browser';
 
 type NotificationEvent = Event & {
-    data: {
-function createNotification(title: string, body: string, icon: string, onClick: () => void, onClose: () => void) {
-    // eslint-disable-next-line no-new
+ * @param onClick - Callback to run on notification click
+ * @param onClose - Callback to run on notification close
+ */
+function createNotification(title: string, body: string, icon: string, onClick: () => void, onClose: () => void): void {
+    const options: NotificationOptions = {
+        body,
+        icon,
+    };
+
+    // Edge browser has issues with requireInteraction, causing notifications to not display
+    if (!Browser.isEdge()) {
+        options.requireInteraction = true;
+    }
+
     const notification = new Notification(title, {
-        requireInteraction: !Browser.isEdge(),
         body,
         icon,
     });
