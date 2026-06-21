440ad5592c91d77e4c57ac2a6c4f5894a95c6159
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-21 00:11:35 -0400

    fix: apply solution for issue #94120

--- src/components/MarkdownTextInput.tsx
+++ src/components/MarkdownTextInput.tsx
@@ -1,11 +1,12 @@
 import {MarkdownTextInput as OriginalMarkdownTextInput} from '@expensify/react-native-live-markdown';
-import type {ClipboardEvent} from '@expensify/react-native-live-markdown/lib/commonjs/MarkdownTextInput';
+import type {MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
 import React, {ForwardedRef, forwardRef} from 'react';
 import type {TextInput} from 'react-native';
 
+
 // eslint-disable-next-line react/function-component-definition
 const MarkdownTextInput = forwardRef<MarkdownTextInputRef, MarkdownTextInputProps>(function MarkdownTextInput(props, ref) {
-    return <OriginalMarkdownTextInput ref={ref} {...props} pasteAsPlainText={false} />;
+    return <OriginalMarkdownTextInput ref={ref} {...props} />;
 });
 
 export default MarkdownTextInput;
