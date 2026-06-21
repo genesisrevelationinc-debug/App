6c43489e024e7f7b78761d4d4cda694ff9125ac8
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-21 00:46:13 -0400

    fix: apply solution for issue #94120

--- src/components/RNTextInput.tsx
+++ src/components/RNTextInput.tsx
@@ -1,34 +1,25 @@
+import type {MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
 import type {ForwardedRef} from 'react';
 import React from 'react';
-import {Platform} from 'react-native';
-import type {TextInput as OriginalTextInput} from 'react-native';
 import {TextInput} from 'react-native';
-import type {CustomTextInputProps, TextInputProps} from './TextInput/BaseTextInput/types';
+import type {TextInputProps} from 'react-native';
+import type {CustomTextInputProps, CustomTextInputRef} from '@components/TextInput/BaseTextInput/types';
 import useLandscapeOnBlurProxy from '@hooks/useLandscapeOnBlurProxy';
 import useTheme from '@hooks/useTheme';
-import type {ForwardedFSClassProps} from '@libs/Fullstory/types';
-import CONST from '@src/CONST';
-
-// Convert the underlying TextInput into an Animated component so that we can take an animated ref and pass it to a worklet
-const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
-
-type AnimatedTextInputRef = typeof AnimatedTextInput & TextInput & HTMLInputElement;
-
-            allowFontScaling={false}
+ * This component is a workaround for the React Native TextInput bug on Android
+ * where the text input doesn't update its value when the value prop changes.
+ */
+function RNTextInput(props: TextInputProps & CustomTextInputProps & MarkdownTextInputProps, ref: ForwardedRef<CustomTextInputRef | HTMLInputElement>) {
+    return (
+        <TextInput
             // eslint-disable-next-line react/jsx-props-no-spreading
-            {...props}
-            // On Android, when pasting text, the text is not automatically formatted with markdown.
-            // This is because the native Android text input doesn't trigger the same events as iOS.
-            // We need to manually trigger the onChangeText event when the text changes via paste.
-            onTextInput={(event) => {
-                if (Platform.OS === 'android' && event.nativeEvent.text) {
-                    props.onChangeText?.(event.nativeEvent.text);
-                }
-                props.onTextInput?.(event);
-            }}
-        />
+
+type RNTextInputWithRefProps = TextInputProps &
     );
 }
+
+export default React.forwardRef(RNTextInput);
+function RNTextInputWithRef({ref, forwardedFSClass = CONST.FULLSTORY.CLASS.UNMASK, ...props}: RNTextInputWithRefProps) {
     const theme = useTheme();
     const inputRef = useRef<AnimatedTextInputRef | null>(null);
     const handleBlur = useLandscapeOnBlurProxy(inputRef, props.onBlur);
