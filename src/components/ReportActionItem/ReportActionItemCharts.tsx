73e710b4426a6314bf224677e7486372270b6a8b
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-26 03:16:08 -0400

    fix: apply solution for issue #91860

---
+++ src/components/ReportActionItem/ReportActionItemCharts.tsx
@@ -0,0 +1,7 @@
+                <VictoryChart
+                    data={chartData}
+                    height={250}
+                    padding={{top: 20, bottom: 60, left: 40, right: 20}}
+                />
+            </View>
+        </View>
