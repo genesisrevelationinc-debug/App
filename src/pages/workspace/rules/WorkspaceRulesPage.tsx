3aa89ac05bb4b4f98a014d5fdb8c3d49b3901597
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-06-28 09:35:01 -0400

    fix: apply solution for issue #94643

--- src/pages/workspace/rules/WorkspaceRulesPage.tsx
+++ src/pages/workspace/rules/WorkspaceRulesPage.tsx
@@ -1,29 +1,11 @@
-import type {TextStyle} from 'react-native';
-import React, {useCallback, useMemo, useState} from 'react';
-import {View} from 'react-native';
-import {useOnyx} from 'react-native-onyx';
-import useLocalize from '@hooks/useLocalize';
-import useNetwork from '@hooks/useNetwork';
-import useThemeStyles from '@hooks/useThemeStyles';
-import useStyleUtils from '@hooks/useStyleUtils';
-import * as PolicyUtils from '@libs/PolicyUtils';
-import * as ReportUtils from '@libs/ReportUtils';
-import Navigation from '@navigation/Navigation';
-    const styles = useThemeStyles();
-    const {translate} = useLocalize();
-    const {isOffline} = useNetwork();
-    const {combineStyles} = useStyleUtils();
-    const [isDeleteRuleModalVisible, setIsDeleteRuleModalVisible] = useState(false);
-    const [isDisableRuleModalVisible, setIsDisableRuleModalVisible] = useState(false);
-    const [isDuplicateNameModalVisible, setIsDuplicateNameModalVisible] = useState(false);
                                                     <Text
                                                         numberOfLines={1}
                                                         ellipsizeMode="tail"
-                                                        style={combineStyles<TextStyle>([
+                                                        style={[
                                                             styles.textStrong,
                                                             styles.flex1,
                                                             styles.mr2,
-                                                        ])}
+                                                        ]}
                                                     >
                                                         {item.name}
                                                     </Text>
