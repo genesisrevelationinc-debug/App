0a14cf6fd831e57a124965addf44cfc584d676d9
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-07-09 16:55:30 -0400

    fix: apply solution for issue #94908

---
+++ src/components/ShareExtension/index.native.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import ShareExtensionNavigator from './ShareExtensionNavigator';
+
+function ShareExtension() {
